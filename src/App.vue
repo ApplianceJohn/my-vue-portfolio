b895f7647926c3048ca22ce73da0deaff9add16c
ApplianceJohn <thednbgremlin@gmail.com>
2022-04-24 12:58:49 -0400

    organized assets and added icon support

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import "bootstrap/js/dist/collapse";
 </script>
 
 <template>
-	<header>
-		<div class="wrapper">
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
-			</nav>
-		</div>
+	<header class="container">
+		<nav class="navbar navbar-expand-lg">
+			<a href="/" class="navbar-brand">
+				<img
+					id="logo"
+					src="@/assets/images/Diamond_Type_Icon.svg"
+					alt="Ben Diamond"
+				/>
+			</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#header-nav"
+				aria-controls="headerNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				Menu
+			</button>
+			<div class="collapse navbar-collapse" id="header-nav">
+				<div class="navbar-nav">
+					<RouterLink to="/" class="nav-link">Home</RouterLink>
+					<RouterLink to="/about" class="nav-link">About</RouterLink>
+					<RouterLink to="/portfolio" class="nav-link"
+						>Portfolio</RouterLink
+					>
+				</div>
+			</div>
+		</nav>
 	</header>
-
-	<RouterView />
+	<main class="container">
+		<RouterView />
+	</main>
 </template>
 
 <style lang="sass">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
-	<main class="container">
+	<div class="home">
 		<h1>Designer. Developer. <span id="etcetera">Etcetera.</span></h1>
 		<p>
 			If you need it, I've probably done it; and if I haven't, I'd love to
 			learn.
 		</p>
-	</main>
+	</div>
 </template>
 
 <style>
